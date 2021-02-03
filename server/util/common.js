2309ee46294e76d80b5089c36ec74fbb3b93f445
ltee <ltketola@gmail.com>
2021-02-03 13:27:44 +0200

    modified port parameters

--- server/util/common.js
+++ server/util/common.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const common = require('../../config/common')
 
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 5000
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
