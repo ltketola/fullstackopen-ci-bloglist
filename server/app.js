5995b584753d0740061874b662ebe74f0c605f14
ltee <ltketola@gmail.com>
2021-02-03 13:57:57 +0200

    modified for deployment to Heroku

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const middleware = require('./middleware/middleware')
 const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const healthRouter = require('./controllers/health')
 
 const app = express()
 
@@ -78,6 +79,7 @@ app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/health', healthRouter)
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
